46301a6d6a12cc4fc70557e592de9d52c6a719f0
Melo <63464796+melisatuun@users.noreply.github.com>
2022-06-20 16:03:19 +0300

    errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Navbar />
     </div>
+        
   );
 }
 
